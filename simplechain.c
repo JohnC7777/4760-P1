7f22761317d38b15853c30141fbf163b5f9a262a
JohnC7777 <73504434+JohnC7777@users.noreply.github.com>
2022-02-07 21:50:25 -0600

    Update simplechain.c

--- simplechain.c
+++ simplechain.c
@@ -7,30 +7,16 @@ int main (int argc, char *argv[]) {
    int i, n;
    int opt;
    
-    while((opt = getopt(argc, argv, ":if:lrx")) != -1) 
-    { 
-        switch(opt) 
-        { 
-            case ‘h’: 
-              printf("Help Message");
-              break;
-            case ‘l’: 
-            case ‘r’: 
-                printf(“option: %c\n”, opt); 
-                break; 
-            case ‘f’: 
-                printf(“filename: %s\n”, optarg); 
-                break; 
-            case ‘:’: 
-                printf(“option needs a value\n”); 
-                break; 
-            case ‘?’: 
-                printf(“unknown option: %c\n”, optopt);
-                break; 
-        } 
-       
-    } 
-
+   while((opt = getopt(argc, argv, "h")) != -1)
+   {
+      switch(opt)
+      {
+            case'h':
+            printf("Help Message");
+            break;
+      }
+   }
+   
    if (argc != 2) {
       fprintf(stderr, "Usage: %s processes\n",argv[0]);
       return 1;
