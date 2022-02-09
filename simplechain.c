b30fd2c39fe113de63dbff4915f9422a3bd6f38d
JohnC7777 <73504434+JohnC7777@users.noreply.github.com>
2022-02-08 20:33:09 -0600

    Update simplechain.c

--- simplechain.c
+++ simplechain.c
@@ -3,6 +3,7 @@
 #include <unistd.h>
 
 int main (int argc, char *argv[]) {
+   //***VARIABLE DECLARATION***
    pid_t childpid = 0;
    int i, n;
    int opt;
@@ -12,6 +13,7 @@ int main (int argc, char *argv[]) {
    int niters=0;
    char str[]="chain: Error: ";
    
+   //***GETOPT***
    while((opt = getopt(argc, argv, "hp:c:s:i:")) != -1)
    {
       switch(opt)
@@ -52,26 +54,40 @@ int main (int argc, char *argv[]) {
       }
    }
    
+   
+   //USED FOR 
    for(i = 1; i < niters; ++i){
       sleep(sleeptime);
    }
-/*
+
+   
+   
+   /*
    char myChar;
    for(i = 1; i<numChars; i++){
       myChar =getc(stdin);
       printf("Next char is: %c \n",myChar);
    }
+ */  
    
+   //USED FOR #4
+   //sleep(10);
    
+   //USED FOR #6
+   //wait();
+   
+   //***PRINT RESULTS***
    fprintf(stderr, "i:%d process ID:%ld parent ID:%ld child ID:%ld\n",
       i, (long)getpid(), (long)getppid(), (long)childpid);
-    */
+    
    
+   
+   /*   USED FOR #7
    fprintf(stderr, "i:%d", i);
    fprintf(stderr, "i:%d process ID:%ld", (long)getpid());
    fprintf(stderr, "parent ID:%ld", (long)getppid());
    fprintf(stderr, "child ID:%ld\n", (long)childpid);
-   
+   */
    
    
    return 0;
