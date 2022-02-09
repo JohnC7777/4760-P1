6a3d86333fedf6ba92d620ed7f4c17f524e6efab
JohnC7777 <73504434+JohnC7777@users.noreply.github.com>
2022-02-08 21:26:23 -0600

    Update simplechain.c

--- simplechain.c
+++ simplechain.c
@@ -78,7 +78,7 @@ int main (int argc, char *argv[]) {
    int offset=numChars*processId;
       
    fseek(stdin, offset-1, SEEK_SET);
-    char mybuf[numChar];
+    char mybuf[numChars];
    for(i = 1; i<numChars; i++){
       myChar =getc(stdin);
       
